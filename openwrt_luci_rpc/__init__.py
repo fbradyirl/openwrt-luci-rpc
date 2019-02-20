8938ef2860bdf193098b455d329101dd79fbbb38
Finbarr Brady <finbarr.brady@gmail.com>
2019-02-20 16:17:27 +0000

    Bump version: 0.4.11 → 0.4.12

--- openwrt_luci_rpc/__init__.py
+++ openwrt_luci_rpc/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Finbarr Brady"""
 __email__ = 'fbradyirl@github.io'
-__version__ = '0.4.11'
+__version__ = '0.4.12'
 
 from .openwrt_luci_rpc import OpenWrtLuciRPC
 from .constants import OpenWrtConstants
--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fbradyirl/openwrt-luci-rpc',
-    version='0.4.11',
+    version='0.4.12',
     zip_safe=False,
 )
