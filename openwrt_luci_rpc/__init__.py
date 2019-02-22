53cf8e81a372a59e86dede04320b2c3b4ef0d8d5
Finbarr Brady <fibrady@cisco.com>
2019-02-22 10:49:44 +0000

    Bump version: 1.0.0 → 1.0.1

--- openwrt_luci_rpc/__init__.py
+++ openwrt_luci_rpc/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Finbarr Brady"""
 __email__ = 'fbradyirl@github.io'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 from .openwrt_luci_rpc import OpenWrtLuciRPC
 from .constants import OpenWrtConstants
