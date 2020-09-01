1e5e086c54e69e710866339017bc5cc87a762bcd
Finbarr Brady <fibrady@cisco.com>
2020-09-01 10:35:56 +0100

    * Unpin pip versions. Allow higher. Fixes #42 https://github.com/fbradyirl/openwrt-luci-rpc/issues/42

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', 'requests==2.21.0', 'packaging==19.1']
+requirements = ['Click>=6.0', 'requests>=2.21.0', 'packaging>=19.1']
 
 setup_requirements = [ ]
 
