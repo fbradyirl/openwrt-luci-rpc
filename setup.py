7409d6b908ef1432f828ab61933573bc2a704082
Finbarr Brady <finbarr.brady@gmail.com>
2023-03-09 20:33:42 +0000

    Bump version: 1.1.15 → 1.1.16

--- setup.py
+++ setup.py
@@ -39,6 +39,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/fbradyirl/openwrt-luci-rpc',
-    version='1.1.15',
+    version='1.1.16',
     zip_safe=False,
 )
